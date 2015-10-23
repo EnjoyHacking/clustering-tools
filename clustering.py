bc8a3b367d6a55651c6224254e1512a02954a52c
syfry <sangyafei1026@gmail.com>
2015-10-23 21:08:22 +0800

    Update

--- clustering.py
+++ clustering.py
@@ -5,6 +5,28 @@ from sklearn.datasets import load_iris
 from sklearn.neighbors import kneighbors_graph
 from sklearn.cluster import SpectralClustering
 
+import os
+
+def groupByAssignment(estimator, filename, protocol):
+	
+	baseDir = "../related_files/" + protocol + "/ClusterResult_" + str(estimator.n_clusters)
+
+	os.mkdir(baseDir)
+
+	for i in xrange(estimator.n_clusters):
+		os.mknod(baseDir + "/cluster_" + str(i) + ".txt");
+
+	predict_labels = estimator.labels_
+
+
+	fsrc = open(filename, "r")
+	for i in xrange(predict_labels.size):
+		fdst = open(baseDir + "/cluster_" + str(predict_labels[i]) + ".txt", "a")
+		fdst.write(fsrc.readline());
+		fdst.close()
+	fsrc.close()
+
+
 def flush(predict_labels, filename):
 	"""
 	@description: Flush data the disk.
@@ -19,8 +41,9 @@ def flush(predict_labels, filename):
 	foutput.close()
 
 if __name__ == '__main__':
+
 	
-	X = np.loadtxt("../related_files/http/http_merge_64_1000_payloadEmbedding.txt", dtype=np.float, delimiter=",")
+	X = np.loadtxt("../related_files/pop3/pop3_pkt_64_2921_payloadEmbedding.txt", dtype=np.float, delimiter=",")
 
 	#iris = load_iris()
 	#data = iris.data
@@ -45,11 +68,13 @@ if __name__ == '__main__':
 	score_kmeans = metrics.silhouette_score(X, estimator_1.labels_, metric="euclidean")
 	score_sc = metrics.silhouette_score(X, estimator_2.labels_, metric="euclidean")
 
-	flush(estimator_2.labels_, "../related_files/http_merge_64_1000_assignment.txt")
+	flush(estimator_2.labels_, "../related_files/pop3/pop3_pkt_64_2921_assignment.txt")
 
 
 	print "silhouette score(kmeans): %f" % score_kmeans
 	print "silhouette score(spectral clustering): %f" % score_sc
+
+	groupByAssignment(estimator_1, "../related_files/pop3/pop3_pkt_64_2921.hex", "pop3")
 
 	
 	
--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Get the vector representation of payload using byte embeddings
-python payload_vectorization.py ../related_files/http/http_merge_64_1000_byteEmbedding.txt ../related_files/http/http_merge_64_1000.hex ../related_files/http/http_merge_64_1000_payloadEmbedding.txt
+#python payload_vectorization.py ../related_files/http/http_merge_64_1000_byteEmbedding.txt ../related_files/http/http_merge_64_1000.hex ../related_files/http/http_merge_64_1000_payloadEmbedding.txt
 
 # Execute k-means clustering algorithm
 #python kmeans.py ../related_files/http/http_merge_64_1000_payloadEmbedding.txt 10 ../related_files/http/http_merge_64_1000_assignment.txt
+
+# Get the vector representation of payload using byte embeddings
+#python payload_vectorization.py ../related_files/pop3/pop3_pkt_64_2921_byteEmbedding.txt ../related_files/pop3/pop3_pkt_64_2921.hex ../related_files/pop3/pop3_pkt_64_2921_payloadEmbedding.txt
+
+# Execute k-means clustering algorithm
+python clustering.py ../related_files/pop3/pop3_pkt_64_2921_payloadEmbedding.txt 10 ../related_files/pop3/pop3_pkt_64_2921_assignment.txt
